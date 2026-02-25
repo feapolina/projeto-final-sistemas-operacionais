7812916c33950f268575eb061da820b907d3f70a
LacerdaGustavo <gustavoandradelacerda@outlook.com>
2026-02-25 16:52:07 -0300

    Salvando meu teste de tela

--- hellocafebabe/kmain.c
+++ hellocafebabe/kmain.c
@@ -1,4 +1,12 @@
-/* kmain.c */
+#include "fb.h" /* Importa as funções que seu amigo criou */
+
 int kmain(void) {
+    /* * A função padrão do livro é: fb_write_cell(posição, caractere, cor_letra, cor_fundo)
+     * Cor 2 = Verde, Cor 0 = Preto
+     */
+    fb_write_cell(0, 'O', 2, 0); 
+    fb_write_cell(1, 'L', 2, 0);
+    fb_write_cell(2, 'A', 2, 0);
+
     return 0xCAFEBABE;
 }
