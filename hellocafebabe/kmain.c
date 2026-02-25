bd0308f7ce1ad0f5af3f71e0b1eaa108d9908239
LacerdaGustavo <gustavoandradelacerda@outlook.com>
2026-02-25 17:27:29 -0300

    Teste

--- hellocafebabe/kmain.c
+++ hellocafebabe/kmain.c
@@ -1,12 +1,18 @@
-#include "fb.h"
-#include "serial.h"
-
 int kmain(void) {
-    /* Criamos a nossa frase */
-    char texto[] = "OLA";
+    /* 1. Criamos o ponteiro direto para a placa de vídeo */
+    char *fb = (char *) 0x000B8000;
     
-    /* Usamos a função do seu amigo: passamos o texto e dizemos que tem 3 letras */
-    fb_write(texto, 3);
-
+    /* 2. Injetamos as letras manualmente na memória 
+       (O código 0x28 significa letra verde com fundo cinza escuro) */
+    
+    fb[0] = 'O';
+    fb[1] = 0x28;
+    
+    fb[2] = 'L';
+    fb[3] = 0x28;
+    
+    fb[4] = 'A';
+    fb[5] = 0x28;
+    
     return 0xCAFEBABE;
 }
