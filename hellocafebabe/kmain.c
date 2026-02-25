96febe79bbc51f5af728846cd10f760cd5ffeffa
LacerdaGustavo <gustavoandradelacerda@outlook.com>
2026-02-25 17:39:25 -0300

    Teste2

--- hellocafebabe/kmain.c
+++ hellocafebabe/kmain.c
@@ -1,18 +1,18 @@
 int kmain(void) {
-    /* 1. Criamos o ponteiro direto para a placa de vídeo */
+    /* 1. Ponteiro direto para a placa de vídeo */
     char *fb = (char *) 0x000B8000;
     
-    /* 2. Injetamos as letras manualmente na memória 
-       (O código 0x28 significa letra verde com fundo cinza escuro) */
+    /* 2. Injetamos as letras manualmente na memória */
+    fb[0] = 'O'; fb[1] = 0x28;
+    fb[2] = 'L'; fb[3] = 0x28;
+    fb[4] = 'A'; fb[5] = 0x28;
     
-    fb[0] = 'O';
-    fb[1] = 0x28;
-    
-    fb[2] = 'L';
-    fb[3] = 0x28;
-    
-    fb[4] = 'A';
-    fb[5] = 0x28;
-    
+    /* 3. A TRAVA MAGICA: Um loop infinito! 
+       Isso faz o processador ficar rodando em círculos aqui para sempre, 
+       mantendo a tela ligada com o nosso texto. */
+    while(1) {
+        /* Não faz nada, só espera */
+    }
+
     return 0xCAFEBABE;
 }
