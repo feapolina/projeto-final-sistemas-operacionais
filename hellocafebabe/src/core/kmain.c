d03ce7159d4a48b30be958583e4c51b897087034
sousademais <isabellasousa2422@gmail.com>
2026-03-06 01:41:19 -0300

    cap7: kernel executa modulo externo carregado pelo GRUB

--- hellocafebabe/src/core/kmain.c
+++ hellocafebabe/src/core/kmain.c
@@ -4,6 +4,7 @@
 #include "gdt.h"
 #include "../interrupts/interrupts.h"
 #include "../interrupts/idt.h"
+#include "multiboot.h"
 
 #define FB_GREEN 2
 #define FB_DARK_GREY 8
@@ -38,6 +39,37 @@ int kmain(unsigned int ebx)
     char msg_serial[] = "Comunicação via porta serial do PC ativa com sucesso!\n";
     serial_write(0x3F8, msg_serial, sizeof(msg_serial) - 1);
 
-    while (1){}
+    /* 4. Interpreta o valor de ebx como ponteiro para a estrutura multiboot_info */
+    multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+
+    /* 5. Verifica se o GRUB carregou módulos */
+    if (!(mbinfo->flags & 0x8)) {
+        char msg_no_modules[] = "ERRO: GRUB nao carregou modulos.\n";
+        fb_write(msg_no_modules, sizeof(msg_no_modules) - 1);
+        while (1) {}
+    }
+
+    /* 6. Verifica se exatamente um módulo foi carregado */
+    if (mbinfo->mods_count != 1) {
+        char msg_mod_count[] = "ERRO: mods_count diferente de 1.\n";
+        fb_write(msg_mod_count, sizeof(msg_mod_count) - 1);
+        while (1) {}
+    }
+
+    /* 7. Obtém o endereço do primeiro módulo carregado */
+    multiboot_module_t *mods = (multiboot_module_t *) mbinfo->mods_addr;
+    unsigned int mod_start = mods[0].mod_start;
+
+    /* 8. Exibe mensagem antes de executar o módulo */
+    char msg_module[] = "Executando modulo...\n";
+    fb_write(msg_module, sizeof(msg_module) - 1);
+
+    /* 9. Trata o endereço do módulo como função e executa */
+    typedef void (*module_entry_t)(void);
+    module_entry_t entry = (module_entry_t) mod_start;
+    entry();
+
+    /* Se o módulo retornar, o kernel entra em loop infinito */
+    while (1) {}
     return 0;
 }
